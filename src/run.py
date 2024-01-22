5973c576a219cd6678269a2fa5a389ecfaeb110c
Tyler Lindsay <tyty464646@gmail.com>
2024-01-22 18:53:56 -0500

    change run.py

--- src/run.py
+++ src/run.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
-in1 = 24
-in2 = 23
+in1 = 20
+in2 = 21
 en = 25
 temp1=1
 
@@ -13,7 +13,7 @@ GPIO.setup(en,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
 p=GPIO.PWM(en,1000)
-p.start(25)
+# p.start(25)
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")

589d76d6af3af5e972710c816ed0f809468bd69c
Tyler Lindsay <tyler@foxdev.net>
2024-01-23 15:11:52 -0500

    websocket stuff

--- src/new_run.py
+++ src/new_run.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask import render_template, request, redirect, jsonify, make_response
+from flask_socketio import SocketIO
 from motors import Motors
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pacifistbd'
+socketio = SocketIO(app)
 m = Motors(20, 21, 16, 12, 1, 7)
 
 @app.route('/control')
 def control():
     return render_template('index.html')
+
+@socketio.on('json')
+def handle_message(json):
+    print('received json: ' + str(json))
 
 def handle_data(x, y):
     # normalize x and y from [-100, 100] to the interval [-1, 1]
@@ -16,12 +23,12 @@ def handle_data(x, y):
 
     m.set_duty_cycle(x, y)
 
-@app.route('/control/data', methods=["POST"])
-def control_data():
-    req = request.get_json()
-    handle_data(req['x'], req['y'])
-    return "Thx brah"
+# @app.route('/control/data', methods=["POST"])
+# def control_data():
+#     req = request.get_json()
+#     handle_data(req['x'], req['y'])
+#     return "Thx brah"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0')
 

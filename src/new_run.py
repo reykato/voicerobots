d3808cb94c022029fe8dedfbe5ecb3b3cd3a8293
Tyler Lindsay <tyty464646@gmail.com>
2024-01-22 20:14:56 -0500

    test flask joystick

--- src/new_run.py
+++ src/new_run.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route('/hello/')
+
+def hello(name=None):
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

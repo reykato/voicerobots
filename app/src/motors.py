98584c384a800f582a32ed37726f5823de55bbe5
Tyler Lindsay <tyler@foxdev.net>
2024-01-24 12:07:14 -0500

    comment and clean up code

--- app/src/motors.py
+++ app/src/motors.py
@@ -2,22 +2,35 @@ import RPi.GPIO as GPIO
 import time
 
 class Motors:
-    def __init__(self, l1, l2, r1, r2, l_en, r_en):
+    def __init__(self, left_1, left_2, right_1, right_2, left_enable, right_enable):
+        '''
+        The Motors object abstracts GPIO initialization and PWM
+        signal management to provide functions for moving the robot.
+
+        Parameters:
+            - left_1 (int): pin which connects to 'in1' on the motor driver
+            - left_2 (int): pin which connects to 'in2' on the motor driver
+            - right_1 (int): pin which connects to 'in3' on the motor driver
+            - right_2 (int): pin which connects to 'in4' on the motor driver
+            - left_enable (int): pin which connects to 'enA' on the motor driver
+            - right_enable (int): pin which connects to 'enB' on the motor driver
+        '''
+
         # l1 and l2 are the two connections to the left motor
-        self.left_1 = l1
-        self.left_2 = l2
+        self.left_1 = left_1
+        self.left_2 = left_2
 
         # l1 and l2 are the two connections to the right motor
-        self.right_1 = r1
-        self.right_2 = r2
+        self.right_1 = right_1
+        self.right_2 = right_2
 
         # l_en is the 'enable' connection to the left motor
         # r_en is the 'enable' connection to the right motor
-        self.left_enable = l_en
-        self.right_enable = r_en
+        self.left_enable = left_enable
+        self.right_enable = right_enable
 
+        # setup motor control pins as outputs
         GPIO.setmode(GPIO.BCM)
-
         GPIO.setup(self.left_1,GPIO.OUT)
         GPIO.setup(self.left_2,GPIO.OUT)
         GPIO.setup(self.right_1,GPIO.OUT)
@@ -41,12 +54,14 @@ class Motors:
         GPIO.output(self.right_2,GPIO.LOW)
     
     def set_duty_cycle(self, x, y):
-        '''Uses the x and y inputs to calculate and output the duty cycle
-          for the motors, controlling the speed and trajectory of the robot
+        '''
+        Uses the x and y inputs to calculate and output the duty cycle
+        for the motors, controlling the speed and trajectory of the robot
           
-          Parameters:
-            x (float): x-axis value (turning)
-            y (float): y-axis value (speed)'''
+        Parameters:
+            - x (float [-1, 1]): x-axis value (turning speed)
+            - y (float [-1, 1]): y-axis value (forward/backward speed)
+        '''
         
         # calculate the duty cycle for each motor, also in interval [-1, 1]
         l_duty_cycle = max(min(1.0, y - x), -1.0)
@@ -78,5 +93,5 @@ class Motors:
         # interval on [0, 1]. the sign only specifies direction
         # which is handled above
         self.left_pwm.ChangeDutyCycle(abs(l_duty_cycle*100))
-        self.right_pwm.ChangeDutyCycle(abs(r_duty_cycle*100))
+        self.rightpwm.ChangeDutyCycle(abs(r_duty_cycle*100))
             
--- app/src/new_run.py
+++ app/src/new_run.py
@@ -3,33 +3,36 @@ from flask import render_template, request, redirect, jsonify, make_response
 from flask_socketio import SocketIO
 from motors import Motors
 
-app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'pacifistbd'
-socketio = SocketIO(app)
-m = Motors(20, 21, 16, 12, 1, 7)
+flask_instance = Flask(__name__)
+socketio = SocketIO(flask_instance) # define websocket object
+motors = Motors(20, 21, 16, 12, 1, 7) # initialize motors with GPIO pin numbers
 
-@app.route('/control')
+@flask_instance.route('/control')
 def control():
     return render_template('joystick.html')
 
+# called when websocket data is sent from the joystick control page
 @socketio.on('json')
 def handle_message(json):
-    # print('received json: ' + str(json))
+    # separate x and y from the json into two variables for easier use
     handle_data(json['x'], json['y'])
 
 def handle_data(x, y):
-    # normalize x and y from [-100, 100] to the interval [-1, 1]
+    '''
+    Normalizes x and y from [-100, 100] to the interval [-1, 1],
+    then calls Motors.set_duty_cycle(x, y) to get the robot to move
+
+    Parameters:
+        - x (int): x position from joystick data
+        - y (int): y position from joystick data
+    '''
     x = float(x) / 100.0
     y = float(y) / 100.0
 
-    m.set_duty_cycle(x, y)
+    motors.set_duty_cycle(x, y)
 
-# @app.route('/joystick/data', methods=["POST"])
-# def control_data():
-#     req = request.get_json()
-#     handle_data(req['x'], req['y'])
-#     return "Thx brah"
-
+# this code is executed when the file is run 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0')
+    # socketio.run starts the websocket service and Flask server
+    socketio.run(flask_instance, debug=True, host='0.0.0.0')
 

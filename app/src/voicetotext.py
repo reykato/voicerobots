9003a689f84e29eebcf42a5dd60405ad5dfcc9ac
gabrielmarx13 <110946716+gabrielmarx13@users.noreply.github.com>
2024-01-24 19:56:53 -0500

    Whisper Speech-to-text & TTS

--- app/src/voicetotext.py
+++ app/src/voicetotext.py
@@ -1,7 +1,21 @@
 import speech_recognition as sr
+import whisper
+import pyttsx3
+import os
 
 with sr.Microphone() as source:
+    print("Listening:")
     audio = sr.Recognizer().listen(source)
 
-with open("microphone-results.wav", "wb") as f:
+with open("app/src/results.wav", "wb") as f:
     f.write(audio.get_wav_data())
+
+model = whisper.load_model("tiny.en")
+result = model.transcribe("app/src/results.wav", fp16 = False)
+print(result["text"])
+
+engine = pyttsx3.init()
+engine.say(result["text"])
+engine.runAndWait()
+
+os.remove("app/src/results.wav")

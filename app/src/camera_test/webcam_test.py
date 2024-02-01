583fbeee2ea5185d4a4372bed3ac08dcd17fb67e
Tyler Lindsay <accounts@foxdev.net>
2024-02-01 12:17:40 -0500

    changed code for webcam test

--- app/src/camera_test/webcam_test.py
+++ app/src/camera_test/webcam_test.py
@@ -1,39 +1,28 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This web application serves a motion JPEG stream
-# main.py
-# import the necessary packages
-from flask import Flask, render_template, Response, request, send_from_directory
-from camera import VideoCamera
-import os
-
-pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
-
-# App Globals (do not edit)
+#Import necessary libraries
+from flask import Flask, render_template, Response
+import cv2
+#Initialize the Flask app
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 @app.route('/')
 def index():
-    return render_template('index.html') #you can customze index.html here
-
-def gen(camera):
-    #get camera frame
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(pi_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# Take a photo when pressing camera button
-@app.route('/picture')
-def take_picture():
-    pi_camera.take_picture()
-    return "None"
-
-if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', debug=False)
+if __name__ == "__main__":
+    app.run(debug=True)

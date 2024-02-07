3ce9e400e49df75034309ab0f1b7bd2dff576513
Tyler Lindsay <tyty464646@gmail.com>
2024-02-06 20:31:10 -0500

    why is it not a function

--- app/src/lidar.py
+++ app/src/lidar.py
@@ -27,7 +27,7 @@ def run():
     # ax.set_rmax(DMAX)
     # ax.grid(True)
 
-    iterator = lidar.iter_measurments('express')
+    iterator = lidar.iter_measurments()
     # ani = animation.FuncAnimation(fig, update_line,
     #     fargs=(iterator, line), interval=50)
     # plt.show()

39c544a1b735908e8e58aa364100fdfc24bc451c
Tyler Lindsay <tyty464646@gmail.com>
2024-02-06 19:55:38 -0500

    that worked

--- app/src/lidar.py
+++ app/src/lidar.py
@@ -1,57 +1,57 @@
-# #!/usr/bin/env python3
-# '''Animates distances and measurment quality'''
-# from rplidar import RPLidar
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import matplotlib.animation as animation
-
-# PORT_NAME = '/dev/ttyUSB0'
-# DMAX = 4000
-# IMIN = 0
-# IMAX = 50
-
-# def update_line(num, iterator, line):
-#     scan = next(iterator)
-#     offsets = np.array([(np.radians(meas[1]), meas[2]) for meas in scan])
-#     line.set_offsets(offsets)
-#     intens = np.array([meas[0] for meas in scan])
-#     line.set_array(intens)
-#     return line,
-
-# def run():
-#     lidar = RPLidar(PORT_NAME)
-#     fig = plt.figure()
-#     ax = plt.subplot(111, projection='polar')
-#     line = ax.scatter([0, 0], [0, 0], s=5, c=[IMIN, IMAX],
-#                            cmap=plt.cm.Greys_r, lw=0)
-#     ax.set_rmax(DMAX)
-#     ax.grid(True)
-
-#     iterator = lidar.iter_scans('express')
-#     ani = animation.FuncAnimation(fig, update_line,
-#         fargs=(iterator, line), interval=50)
-#     plt.show()
-#     lidar.stop()
-#     lidar.disconnect()
-
-# if __name
-# __ == '__main__':
-#     run()
-
+#!/usr/bin/env python3
+'''Animates distances and measurment quality'''
 from rplidar import RPLidar
-lidar = RPLidar('/dev/ttyUSB0')
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
-info = lidar.get_info()
-print(info)
+PORT_NAME = '/dev/ttyUSB0'
+DMAX = 4000
+IMIN = 0
+IMAX = 50
 
-health = lidar.get_health()
-print(health)
+def update_line(num, iterator, line):
+    scan = next(iterator)
+    offsets = np.array([(np.radians(meas[1]), meas[2]) for meas in scan])
+    line.set_offsets(offsets)
+    intens = np.array([meas[0] for meas in scan])
+    line.set_array(intens)
+    return line,
 
-for i, scan in enumerate(lidar.iter_scans()):
-    print('%d: Got %d measurments' % (i, len(scan)))
-    if i > 10:
-        break
+def run():
+    lidar = RPLidar(PORT_NAME)
+    fig = plt.figure()
+    ax = plt.subplot(111, projection='polar')
+    line = ax.scatter([0, 0], [0, 0], s=5, c=[IMIN, IMAX],
+                           cmap=plt.cm.Greys_r, lw=0)
+    ax.set_rmax(DMAX)
+    ax.grid(True)
 
-lidar.stop()
-lidar.stop_motor()
-lidar.disconnect()
+    iterator = lidar.iter_scans('express')
+    ani = animation.FuncAnimation(fig, update_line,
+        fargs=(iterator, line), interval=50)
+    plt.show()
+    lidar.stop()
+    lidar.disconnect()
+
+if __name
+__ == '__main__':
+    run()
+
+# from rplidar import RPLidar
+# lidar = RPLidar('/dev/ttyUSB0')
+
+# info = lidar.get_info()
+# print(info)
+
+# health = lidar.get_health()
+# print(health)
+
+# for i, scan in enumerate(lidar.iter_scans()):
+#     print('%d: Got %d measurments' % (i, len(scan)))
+#     if i > 10:
+#         break
+
+# lidar.stop()
+# lidar.stop_motor()
+# lidar.disconnect()

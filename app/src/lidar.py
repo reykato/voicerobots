c3f32ad87cea9e99bd54db53cf701e07598425af
Tyler Lindsay <tyty464646@gmail.com>
2024-02-06 20:36:54 -0500

    ok it works but its slow

--- app/src/lidar.py
+++ app/src/lidar.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
 '''Animates distances and measurment quality'''
 from rplidar import RPLidar
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
 PORT_NAME = '/dev/ttyUSB0'
 
 
+PORT_NAME = '/dev/ttyUSB0'
+DMAX = 4000
+IMIN = 0
+IMAX = 50
 
+def update_line(num, iterator, line):
+    scan = next(iterator)
+    offsets = np.array([(np.radians(meas[1]), meas[2]) for meas in scan])
+    line.set_offsets(offsets)
+    intens = np.array([meas[0] for meas in scan])
+    line.set_array(intens)
+    return line,
 
 def run():
     lidar = RPLidar(PORT_NAME)
 
 
-    for i, scan in enumerate(lidar.iter_scans()):
-        print('%d: Got %d measurments' % (i, len(scan)))
-        for s in scan:
-            print(s)
-        if i > 30:
-            break
+    # for i, scan in enumerate(lidar.iter_scans()):
+    #     print('%d: Got %d measurments' % (i, len(scan)))
+    #     for s in scan:
+    #         print(s)
+
+    fig = plt.figure()
+    ax = plt.subplot(111, projection='polar')
+    line = ax.scatter([0, 0], [0, 0], s=5, c=[IMIN, IMAX],
+                           cmap=plt.cm.Greys_r, lw=0)
+    ax.set_rmax(DMAX)
+    ax.grid(True)
+
+    iterator = lidar.iter_scans()
+    ani = animation.FuncAnimation(fig, update_line,
+        fargs=(iterator, line), interval=10)
+    plt.show()
 
     lidar.stop()
     lidar.disconnect()

627d98840045406d0685f39d3405f2ecad78c3d8
Tyler Lindsay <tyty464646@gmail.com>
2024-02-06 20:17:48 -0500

    exaplmlepl

--- app/src/lidar.py
+++ app/src/lidar.py
@@ -1,26 +1,30 @@
 from pyrplidar import PyRPlidar
+import time
 
-lidar = PyRPlidar()
-lidar.connect(port="/dev/ttyUSB0", baudrate=256000, timeout=3)
-# Linux   : "/dev/ttyUSB0"
-# MacOS   : "/dev/cu.SLAB_USBtoUART"
-# Windows : "COM5"
+def simple_express_scan():
+
+    lidar = PyRPlidar()
+    lidar.connect(port="/dev/ttyUSB0", baudrate=256000, timeout=3)
+    # Linux   : "/dev/ttyUSB0"
+    # MacOS   : "/dev/cu.SLAB_USBtoUART"
+    # Windows : "COM5"
+
+                  
+    lidar.set_motor_pwm(500)
+    time.sleep(2)
+    
+    scan_generator = lidar.start_scan_express(4)
+    
+    for count, scan in enumerate(scan_generator()):
+        print(count, scan)
+        if count == 20: break
+
+    lidar.stop()
+    lidar.set_motor_pwm(0)
+
+    
+    lidar.disconnect()
 
 
-info = lidar.get_info()
-print("info :", info)
-
-health = lidar.get_health()
-print("health :", health)
-
-samplerate = lidar.get_samplerate()
-print("samplerate :", samplerate)
-
-
-scan_modes = lidar.get_scan_modes()
-print("scan modes :")
-for scan_mode in scan_modes:
-    print(scan_mode)
-
-
-lidar.disconnect()
+if __name__ == "__main__":
+    simple_express_scan()

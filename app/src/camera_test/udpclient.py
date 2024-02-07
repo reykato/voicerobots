811b7c6660cf29eafba24d9a1035e5a7ac9eb6e2
Tyler Lindsay <tyty464646@gmail.com>
2024-02-07 01:23:22 -0500

    480p is fine i guess

--- app/src/camera_test/udpclient.py
+++ app/src/camera_test/udpclient.py
@@ -13,8 +13,8 @@ port = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 cap = cv2.VideoCapture(0)
-ret = cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-ret = cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+ret = cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+ret = cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 ret, frame = cap.read()
 

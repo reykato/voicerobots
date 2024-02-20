f4c07b08f768e13ebbdfa435b332cfae8ca68784
Tyler Lindsay <tyty464646@gmail.com>
2024-02-20 17:58:46 -0500

    adjust server side code

--- app/src/controlstreamhandler.py
+++ app/src/controlstreamhandler.py
@@ -5,12 +5,13 @@ from motors import Motors
 
 class ControlStreamHandler(StreamHandler):
     socket = None
+    client_socket = None
     motors = Motors(20, 21, 16, 12, 1, 7) # initialize motors with GPIO pin numbers
 
     def _handle_stream(self):
         while not self.stop_event.is_set():
             try:
-                received_data = self.socket.recv(1024)
+                received_data = self.client_socket.recv(1024)
             except socket.error as e:
                 received_data = None
                 if not e.args[0] == 'timed out':
@@ -22,14 +23,15 @@ class ControlStreamHandler(StreamHandler):
                 print(f"Received: {decoded_data}")
 
                 # Echo back the received data
-                self.socket.sendall(decoded_data)
+                self.client_socket.sendall(decoded_data)
                 self.motors.set_duty_cycle(decoded_data[0], decoded_data[1])
 
     def _before_starting(self):
         socket.timeout(0.005) # socket will stop waiting for packets after 5ms
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.host, self.port))
+        self.socket.bind((self.host, self.port))
         self.socket.listen(4)
+        self.client_socket, _ = self.socket.accept()
     
     def _after_stopping(self):
         self.socket.close()

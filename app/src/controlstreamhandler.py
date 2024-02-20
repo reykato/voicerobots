a7fff448c1d5cc71e38b2252052690eece0f0514
Tyler Lindsay <accounts@foxdev.net>
2024-02-20 15:04:58 -0500

    make socket non-blocking

--- app/src/controlstreamhandler.py
+++ app/src/controlstreamhandler.py
@@ -13,8 +13,9 @@ class ControlStreamHandler(StreamHandler):
                 received_data = self.socket.recv(1024)
             except socket.error as e:
                 received_data = None
-                if not e.args[0] == errno.EAGAIN and e.args[0] == errno.EWOULDBLOCK:
+                if not e.args[0] == 'timed out':
                     print(err = e.args[0])
+                    self.stop()
 
             if not received_data == None:
                 decoded_data = received_data.decode()
@@ -24,12 +25,11 @@ class ControlStreamHandler(StreamHandler):
                 self.socket.sendall(decoded_data)
                 self.motors.set_duty_cycle(decoded_data[0], decoded_data[1])
 
-
-    
     def _before_starting(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.timeout(0.005) # socket will stop waiting for packets after 5ms
         self.socket.bind((self.host, self.port))
-        self.socket.listen(5)
+        self.socket.listen()
     
     def _after_stopping(self):
         self.socket.close()

fd34e1c4c73fcb66634f1534609f7ad4dc2c79c2
Tyler Lindsay <tyty464646@gmail.com>
2024-02-29 12:25:23 -0500

    change test to go forward and backwards

--- app/src/testmotors.py
+++ app/src/testmotors.py
@@ -3,6 +3,6 @@ import time
 m = Motors(20, 21, 12, 16)
 
 while True:
-    m.set_stepper_speed(1,1)
+    m.set_stepper_speed(0,1)
     time.sleep(1)
-    m.set_stepper_speed(0,0)
+    m.set_stepper_speed(0,-1)
